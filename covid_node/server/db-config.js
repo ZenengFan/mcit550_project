9c5a3d413b9604420940287bdfc7c8c0b42e1ab8
Zeneng Fan <fanzeneg@gmail.com>
2021-04-24 16:19:47 -0400

    remove unused code

--- covid_node/server/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private. Will not be included in submission
-module.exports = {
-  host: "cis550-proj.chge9gphb71b.us-east-1.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "CIS550_Upenn",
-  database: "cis550proj"
-};
